75ec54955b68ee829cdc6aa45f07c3f393fab889
jackn28 <115190376+jackn28@users.noreply.github.com>
2024-01-13 09:23:38 -0600

    added gyro reset button for swerve

--- src/robot.py
+++ src/robot.py
@@ -1,22 +1,22 @@
+from numpy import newaxis
+from inputs import nonLinearScaler
 import robotHAL
 import swerveDrive
 import timing
 import wpilib
+from wpimath.geometry import Translation2d
 from ntcore import NetworkTableInstance
 from wpimath.geometry import Pose2d, Rotation2d
 from wpimath.kinematics import ChassisSpeeds, SwerveModulePosition
-from wpimath.trajectory import TrajectoryUtil
-from wpimath.controller import HolonomicDriveController
-from wpimath.controller import PIDController, ProfiledPIDControllerRadians
-from wpimath.trajectory import TrapezoidProfile
 
 
 class RobotInputs():
     def __init__(self, drive: wpilib.XboxController, arm: wpilib.XboxController) -> None:
-        self.driveX = drive.getLeftX()
-        self.driveY = drive.getLeftY()
-        self.turning = drive.getRightX()
-
+        self.driveX = nonLinearScaler(drive.getLeftX())
+        self.driveY = nonLinearScaler(drive.getLeftY())
+        self.turning = nonLinearScaler(drive.getRightX())
+        self.absoluteDriveSwitch = drive.getXButton()
+        self.gyroReset = drive.getYButton()
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self) -> None:
@@ -31,10 +31,10 @@ class Robot(wpilib.TimedRobot):
         self.hal = robotHAL.RobotHALBuffer()
         self.hardware = robotHAL.RobotHAL()
         self.hardware.update(self.hal)
-
+        self.absoluteDrive = -1
         # (Rob): I would have built the SwerveModulePosition class directly into the HALBuffer, if it weren't for the fact that python can't 'pickle' them. (???)
         self.drive = swerveDrive.SwerveDrive(Rotation2d(0), Pose2d(0, 0, 0),
-                                             [SwerveModulePosition(self.hal.drivePositions[i], Rotation2d(self.hal.steeringPositions[i])) for i in range(4)])
+            [SwerveModulePosition(self.hal.drivePositions[i], Rotation2d(self.hal.steeringPositions[i])) for i in range(4)])
 
     def robotPeriodic(self) -> None:
         self.time = timing.TimeData(self.time)
@@ -45,46 +45,32 @@ class Robot(wpilib.TimedRobot):
 
     def teleopPeriodic(self) -> None:
         self.input = RobotInputs(self.driveCtrlr, self.armCtrlr)
+        currentYaw = self.hal.yaw
+        
+        #newX = X*math.cos(-1*self.currentYaw)
+        #newY = 
+        if(self.input.absoluteDriveSwitch):
+                self.absoluteDrive = self.absoluteDrive*-1
+        if(self.input.gyroReset):
+            self.hal.yaw = 0
+            
+        if(self.absoluteDrive == 1):
+            originalVector = Translation2d(self.input.driveX,self.input.driveY)
+            rotation = Rotation2d(currentYaw)
+            newVector = originalVector.rotateBy(rotation)
+            speed = ChassisSpeeds(newVector.X() * 0.1, newVector.Y() * 0.1, -self.input.turning * 0.1)
+        else:
+            speed = ChassisSpeeds(self.input.driveX * 0.1, self.input.driveY * 0.1, -self.input.turning * 0.1)
 
-        speed = ChassisSpeeds(self.input.driveX * 0.1,
-                              self.input.driveY * 0.1, -self.input.turning * 0.1)
         self.drive.update(self.time.dt, self.hal, speed)
 
         self.hardware.update(self.hal)
 
     def autonomousInit(self) -> None:
-        self.autoTimer = timing.TimeData(None)
-        XControllerP = 1
-        XControllerI = 0
-        XControllerD = 0
-        YControllerP = 1
-        YControllerI = 0
-        YControllerD = 0
-        RControllerP = 1
-        RControllerI = 0
-        RControllerD = 0
-        T_PConstraintsVolocityMax = 6.28
-        T_PConstraintsRotaionAccelerationMax = 1
-        self.XController = PIDController(
-            XControllerP, XControllerI, XControllerD)
-        self.YController = PIDController(
-            YControllerP, YControllerI, YControllerD)
-        self.RotationController = ProfiledPIDControllerRadians(
-            RControllerP, RControllerI, RControllerD, TrapezoidProfile.Constraints(T_PConstraintsVolocityMax, T_PConstraintsRotaionAccelerationMax))
-        trajectoryJSON = "deploy/path.wpilib.json"
-        self.trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryJSON)
-        self.holonomicController = HolonomicDriveController(
-            self.XController, self.YController, self.RotationController)
+        pass
 
     def autonomousPeriodic(self) -> None:
-        trajectoryHeadingAngle = 0
-        # self.hal.stopMotors()
-        CurrentPose = self.drive.odometry.getPose()
-        self.autoTImer = timing.TimeData(self.autoTimer)
-        goal = self.trajectory.sample(self.autoTimer.timeSinceInit)
-        adjustedSpeeds = self.holonomicController.calculate(
-            CurrentPose, goal, Rotation2d(trajectoryHeadingAngle))
-        self.drive.update(self.autoTimer.dt, self.hal, adjustedSpeeds)
+        self.hal.stopMotors()
         self.hardware.update(self.hal)
 
     def disabledInit(self) -> None:
@@ -93,7 +79,6 @@ class Robot(wpilib.TimedRobot):
     def disabledPeriodic(self) -> None:
         self.hal.stopMotors()
         self.hardware.update(self.hal)
-
 
 if __name__ == "__main__":
     wpilib.run(Robot)
