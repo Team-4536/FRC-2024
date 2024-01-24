fe8da631ed9203cb930ff8657e5c44d45315f04c
emmett <emmett@christensons.net>
2024-01-23 19:34:24 -0600

    good old testing

--- src/inputs.py
+++ src/inputs.py
@@ -2,6 +2,6 @@
 # TODO: nonlinear, and fix the jump with this one
 def deadZone(input: float) -> float:
         if(abs(input) < 0.1):
-            return 0.0 #This is a test --DJO
+            return 0.0
         else:
             return float(input)
