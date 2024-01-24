ecfc5e171e65d7cb9be13182ef91b9df84057a11
emmett <emmett@christensons.net>
2024-01-23 19:00:40 -0600

    fixed Scaler stuff

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import math
 
-class scaler:
+class Scaler:
     def __init__(self, deadZone = .1, power = 1):
         self.deadZone = deadZone
         self.power = power
@@ -10,10 +10,12 @@ class scaler:
             return 0
         else:
             return ((abs(input) - self.deadZone)/(1 - self.deadZone))**self.power * math.copysign(1, input)
+        
+    def __call__(self, input):
+        return self.scale(input)
     
     def setDeadZone(self, deadZone):
         self.deadZone = deadZone
     
     def setPower(self, power):
         self.power = power
-        
