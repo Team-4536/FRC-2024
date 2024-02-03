7bd80deb10bbb4cce6fb27db371190f29e294c44
Robbie <robbiemaldrich@gmail.com>
2024-02-03 12:09:20 -0600

    inital commit

--- src/robot.py
+++ src/robot.py
@@ -1,95 +1,18 @@
-from gettext import translation
-from xml.sax.xmlreader import InputSource
 import wpilib
-from ntcore import NetworkTableInstance
-from wpimath.geometry import Pose2d, Rotation2d
-from wpimath.kinematics import ChassisSpeeds, SwerveModulePosition
-from utils import Scalar
-from phoenix6.hardware import CANcoder
-import robotHAL
-from ntcore import NetworkTableInstance
-from wpimath.kinematics import SwerveModuleState
-import math
-from swerveDrive import SwerveDrive
-from timing import TimeData
-from real import lerp
-from wpimath.geometry import Translation2d
-
-
-class RobotInputs():
-    def __init__(self, drive: wpilib.XboxController, arm: wpilib.XboxController) -> None:
-        self.xScalar = Scalar(deadZone = .1, exponent = 1)
-        self.yScalar = Scalar(deadZone = .1, exponent = 1)
-        self.rotScalar = Scalar(deadZone = .1, exponent = 1)
-
-
-        ##flipped x and y inputs so they are relative to bot
-        self.driveX: float = self.xScalar(-drive.getLeftY())
-        self.driveY: float = self.yScalar(-drive.getLeftX())
-        self.turning: float = self.rotScalar(drive.getRightX())
-
-        self.speedCtrl: float = drive.getRightTriggerAxis()
-
-        self.gyroReset: bool = drive.getYButtonPressed()
-        self.brakeButton: bool = drive.getBButtonPressed()
-        self.absToggle: bool = drive.getXButtonPressed()
 
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self) -> None:
-        self.hal = robotHAL.RobotHALBuffer()
-        self.hardware = robotHAL.RobotHAL()
-        self.hardware.update(self.hal)
-
-        self.table = NetworkTableInstance.getDefault().getTable("telemetry")
-
-        self.driveCtrlr = wpilib.XboxController(0)
-        self.armCtrlr = wpilib.XboxController(1)
-        self.input = RobotInputs(self.driveCtrlr, self.armCtrlr)
-
-        wheelPositions = [SwerveModulePosition(self.hal.drivePositions[i], Rotation2d(self.hal.steeringPositions[i])) for i in range(4)]
-        self.drive = SwerveDrive(Rotation2d(self.hal.yaw), Pose2d(), wheelPositions)
-        self.time = TimeData(None)
-
-        self.abs = True
-
+        pass
 
     def robotPeriodic(self) -> None:
-        self.time = TimeData(self.time)
-        self.hal.publish(self.table)
-        self.drive.updateOdometry(self.hal)
+        pass
 
     def teleopInit(self) -> None:
         pass
 
     def teleopPeriodic(self) -> None:
-        self.input = RobotInputs(self.driveCtrlr, self.armCtrlr)
-
-        if self.input.gyroReset:
-            self.hal.yaw = 0
-
-        if self.input.absToggle:
-            self.abs = not self.abs
-
-        defaultSpeed = 1
-        maxSpeed = 4
-        speedControlEdited = lerp(defaultSpeed, maxSpeed, self.input.speedCtrl)
-        turnScalar = 3
-
-        driveVector = Translation2d(self.input.driveX * speedControlEdited, self.input.driveY * speedControlEdited)
-        driveVector = driveVector.rotateBy(Rotation2d(-self.hal.yaw))
-
-        if self.abs:
-            speed = ChassisSpeeds(driveVector.X(), driveVector.Y(), -self.input.turning * turnScalar)
-        else:
-            speed = ChassisSpeeds(self.input.driveX * speedControlEdited, self.input.driveY * speedControlEdited, -self.input.turning * turnScalar)
-
-        pose = self.drive.odometry.getPose()
-        self.table.putNumber("odomX", pose.x )
-        self.table.putNumber("odomY", pose.y)
-
-        self.drive.update(self.time.dt, self.hal, speed)
-        self.hardware.update(self.hal)
+        pass
 
     def autonomousInit(self) -> None:
         pass
@@ -98,12 +21,10 @@ class Robot(wpilib.TimedRobot):
         pass
 
     def disabledInit(self) -> None:
-        self.disabledPeriodic()
+        pass
 
     def disabledPeriodic(self) -> None:
-        self.hal.stopMotors()
-        self.hardware.update(self.hal)
-
+        pass
 
 if __name__ == "__main__":
     wpilib.run(Robot)
