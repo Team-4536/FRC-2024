1b4d4d889c47a708f47800b05cbff1c59d773bf3
tryhardcoder <51182105+tryhardcoder@users.noreply.github.com>
2024-02-06 18:17:49 -0600

    Hcpa (#26)
    Co-authored-by: Little Sack <mentocylex@gmail.com>
    Co-authored-by: Michael Foley <mfoley001@stpaul.k12.mn.us>

--- src/robot.py
+++ src/robot.py
@@ -1,28 +1,26 @@
-from gettext import translation
-from xml.sax.xmlreader import InputSource
-import wpilib
-from ntcore import NetworkTableInstance
-from wpimath.geometry import Pose2d, Rotation2d
-from wpimath.kinematics import ChassisSpeeds, SwerveModulePosition
-from utils import Scalar
-from phoenix6.hardware import CANcoder
+
 import robotHAL
+import wpilib
+from mechanism import Mechanism
 from ntcore import NetworkTableInstance
-from wpimath.kinematics import SwerveModuleState
-import math
+from real import lerp
 from swerveDrive import SwerveDrive
 from timing import TimeData
-from real import lerp
-from wpimath.geometry import Translation2d
+from utils import Scalar
+from wpimath.geometry import Pose2d, Rotation2d, Translation2d
+from wpimath.kinematics import ChassisSpeeds, SwerveModulePosition
 
 
 class RobotInputs():
     def __init__(self, drive: wpilib.XboxController, arm: wpilib.XboxController) -> None:
+        # scalars
         self.xScalar = Scalar(deadZone = .1, exponent = 1)
         self.yScalar = Scalar(deadZone = .1, exponent = 1)
         self.rotScalar = Scalar(deadZone = .1, exponent = 1)
+        self.intakeScalar = Scalar(deadZone = .1, exponent = 1)
+        self.testScalar = Scalar(deadZone = .1, exponent = 1)
 
-
+        # drive
         ##flipped x and y inputs so they are relative to bot
         self.driveX: float = self.xScalar(-drive.getLeftY())
         self.driveY: float = self.yScalar(-drive.getLeftX())
@@ -33,6 +31,14 @@ class RobotInputs():
         self.gyroReset: bool = drive.getYButtonPressed()
         self.brakeButton: bool = drive.getBButtonPressed()
         self.absToggle: bool = drive.getXButtonPressed()
+
+        # arm controller
+        self.intake: bool = arm.getAButton()
+        self.shootSpeaker: bool = arm.getYButton()
+        self.shootAmp: bool = arm.getBButton()
+        self.shooterIntake: bool = arm.getLeftBumper()
+
+        self.shooterJoystick: float = self.testScalar(-arm.getRightY())
 
 
 class Robot(wpilib.TimedRobot):
@@ -52,6 +58,8 @@ class Robot(wpilib.TimedRobot):
         self.time = TimeData(None)
 
         self.abs = True
+
+        self.mech = Mechanism(self.hal)
 
 
     def robotPeriodic(self) -> None:
@@ -83,12 +91,29 @@ class Robot(wpilib.TimedRobot):
             speed = ChassisSpeeds(driveVector.X(), driveVector.Y(), -self.input.turning * turnScalar)
         else:
             speed = ChassisSpeeds(self.input.driveX * speedControlEdited, self.input.driveY * speedControlEdited, -self.input.turning * turnScalar)
+        self.drive.update(self.time.dt, self.hal, speed) # commented for mech testing
 
         pose = self.drive.odometry.getPose()
         self.table.putNumber("odomX", pose.x )
         self.table.putNumber("odomY", pose.y)
 
-        self.drive.update(self.time.dt, self.hal, speed)
+        if self.input.intake:
+            for i in range(2):
+                self.hal.intakeSpeeds[i] = 0.4
+        else:
+            self.hal.intakeSpeeds[0] = 0
+            self.hal.intakeSpeeds[1] = 0
+
+        #shooter
+        if self.input.shootSpeaker:
+            self.hal.shooterSpeed = 0.25
+
+        if self.input.shootAmp:
+            self.hal.shooterSpeed = 0.1
+
+        if self.input.shooterIntake:
+            self.hal.shooterIntakeSpeed = 0.1
+
         self.hardware.update(self.hal)
 
     def autonomousInit(self) -> None:
