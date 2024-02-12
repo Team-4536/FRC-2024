16e320b99f1d969db6f507b8a611daa230e0c2e4
Little Sack <mentocylex@gmail.com>
2024-02-12 16:50:27 -0600

    arm pid testing setup

--- src/shooterStateMachine.py
+++ src/shooterStateMachine.py
@@ -12,6 +12,7 @@ class StateMachine():
     SHOOTING = 3
 
     SHOOTER_SCALER = 0.1
+    AIM_SCALER = 0.1
 
     # 0 is target aim, 1 is target speeds
     ampSetpoint = (0, 0)
@@ -22,10 +23,15 @@ class StateMachine():
         self.table = NetworkTableInstance.getDefault().getTable("ShooterStateMachineSettings")
         self.table.putNumber("kff", 0.00181)
         self.table.putNumber("kp", 0.0008)
+        self.table.putNumber("Arm kp", 0)
+        self.table.putNumber("Arm kg", 0)
+        self.table.putNumber("armTarget", 0)
+        self.table.putNumber("shooter scaler", 0)
 
         self.aimSetpoint = 0
         self.speedSetpoint = 0
-        self.speedSetpointActual = 0
+        self.PIDspeedSetpoint = 0
+        self.PIDaimTarget = 0
         self.state = self.READY_FOR_RING
 
         self.aimPID = PIDControllerForArm(0, 0, 0, 0, 0, 0)
@@ -36,6 +42,9 @@ class StateMachine():
     def update(self, hal: RobotHALBuffer, inputAmp: bool, inputPodium: bool, inputSubwoofer: bool, inputShoot: bool, time: float, dt: float):
         self.shooterPID.kff = self.table.getNumber("kff", 0)
         self.shooterPID.kp = self.table.getNumber("kp", 0)
+        self.aimPID.kp = self.table.getNumber("Arm kp", 0)
+        self.aimPID.kg = self.table.getNumber("Arm kg", 0)
+        self.SHOOTER_SCALER = self.table.getNumber("shooter scaler", 0)
 
         if(inputAmp):
             if(self.state == self.READY_FOR_RING):
@@ -69,14 +78,17 @@ class StateMachine():
                 self.state = self.AIMING
 
         elif(self.state == self.AIMING):
-            aimTarget = self.aimSetpoint
+            #aimTarget = self.aimSetpoint
             speedTarget = self.speedSetpoint
+
+            aimTarget = self.table.getNumber("armTarget", 0)
+
             if(inputShoot):
                 self.state = self.SHOOTING
                 self.time = time
 
         elif(self.state == self.SHOOTING):
-            aimTarget = self.aimSetpoint
+            aimTarget = self.table.getNumber("armTarget", 0) #self.aimSetpoint
             speedTarget = self.speedSetpoint
             hal.shooterIntakeSpeed = 0.4
             hal.intakeSpeeds[1] = 0.4
@@ -88,8 +100,10 @@ class StateMachine():
             speedTarget = 0
 
         # hal.shooterAimSpeed = self.aimPID.tick(self.aimSetpoint, hal.shooterAimPos, dt)
-        self.speedSetpointActual = (speedTarget - self.speedSetpointActual) * self.SHOOTER_SCALER + self.speedSetpointActual
-        hal.shooterAimSpeed = aimTarget
-        hal.shooterSpeed = self.shooterPID.tick(self.speedSetpointActual, hal.shooterAngVelocityMeasured, dt)
+        self.PIDspeedSetpoint = (speedTarget - self.PIDspeedSetpoint) * self.SHOOTER_SCALER + self.PIDspeedSetpoint
+        self.PIDaimTarget = (aimTarget - self.PIDaimTarget) * self.AIM_SCALER + self.PIDaimTarget
+        self.table.putNumber("PIDarmTarget", self.PIDaimTarget)
+        hal.shooterAimSpeed = self.aimPID.tick(self.PIDaimTarget, hal.shooterAimPos, dt)
+        hal.shooterSpeed = self.shooterPID.tick(self.PIDspeedSetpoint, hal.shooterAngVelocityMeasured, dt)
 
         return self.state
