afdb91ab171a6139975aff602ddc72410dea390e
Mark Christenson <mark@christensons.net>
2024-02-13 19:50:17 -0600

    Scaler test (#30)

--- src/tests/pyfrc_test.py
+++ src/tests/pyfrc_test.py
@@ -3,9 +3,6 @@
     to test basic functionality of just about any robot.
 '''
 
-import pytest
-import pytest_reraise
-
 # from pyfrc.tests.docstring_test import test_docstrings
 
 from pyfrc.tests import *
