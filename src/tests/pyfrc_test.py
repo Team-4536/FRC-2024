b815ea7a37c1ea2e5667bc0ced2aaa658e6dee0e
davidorser <davidorser@gmail.com>
2024-03-02 13:38:31 -0600

    Adding comments that describe what FRC tests does.  Included a link to website.

--- src/tests/pyfrc_test.py
+++ src/tests/pyfrc_test.py
@@ -6,3 +6,8 @@
 # from pyfrc.tests.docstring_test import test_docstrings
 
 from pyfrc.tests import *
+
+# pyfrc.test run through disabled, teleop, auto, then back to disabled and looks for crash bugs.  Doesn't test inputs or much else.
+# We can write additional tests to verify our code still works after changes.
+
+# You can read more about pyfrc.test's here: https://robotpy.readthedocs.io/projects/pyfrc/en/stable/testing.html
