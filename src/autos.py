dad9f63c5e75cd4f12843019d0a1d81b31a2af71
bea <martin.bea.c@icloud.com>
2024-04-05 14:11:57 -0500

    added LL timeout

--- src/autos.py
+++ src/autos.py
@@ -231,6 +231,8 @@ class AutoBuilder:
                 #X, Y, & Yaw are updated correctly
                 r.drive.resetOdometry(visionPose2D, r.hal)
                 return True
+            elif (r.time.timeSinceInit - r.auto.stageStart) > 0.25:
+                return True
             return False
         
         self.add(Stage(func, "reset odom with limelight"))
