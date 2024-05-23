b1db91577d1fe77e561137ef51b26703c493eba9
davidorser <davidorser@gmail.com>
2024-05-23 12:53:17 -0500

    Added simple example of single custom button

--- Arduino/djo_RP2040CAN_quickJoystick/djo_RP2040CAN_quickJoystick.ino
+++ Arduino/djo_RP2040CAN_quickJoystick/djo_RP2040CAN_quickJoystick.ino
@@ -12,6 +12,8 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Use BOOTSEL to start the Joystick demo.");
   Joystick.begin();
+
+  pinMode(5,INPUT_PULLUP);
 }
 
 void loop() {
@@ -24,10 +26,20 @@ void loop() {
   if (!digitalRead(PIN_BUTTON)) { //Check the BOOTSEL button state (pulled-up --> FALSE=pressed)
     Serial.println("Joystick buttons");
     for (int i = 1; i <= 32; i++) {
+      Serial.print(i);
+      Serial.print(",");
       Joystick.button(i, true);
       delay(250);
       Joystick.button(i, false);
       delay(10); // We need a short delay here, sending packets with less than 1ms leads to packet loss!
     }
   }
+
+  // Check manual button (needs to be installed on breadboard.)
+  if (!digitalRead(5)) { //Check the Externally Wired Custom Button state (pulled-up --> FALSE=pressed)
+   Joystick.button(3, true);
+  } else {
+   Joystick.button(3, false);
+  }
+  delay(10); // We need a short delay here, sending packets with less than 1ms leads to packet loss!
 }
