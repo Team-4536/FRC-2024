58a6707e1be2e52d354a6bac78ab87379c9338e3
bea <martin.bea.c@icloud.com>
2024-06-04 17:01:53 -0500

    added perfLog to gitignore, appears to work

--- src/profiler.py
+++ src/profiler.py
@@ -2,6 +2,11 @@ from typing import Callable
 import wpilib
 from ntcore import NetworkTableInstance
 
+
+# Data for the state of this module - dont mess with please
+# accumulates samples recorded with profileRegion and profileFn
+_completeSamples: list['__Sample'] = []
+
 class __Sample():
     def __init__(self, name: str) -> None:
         self.startTime: float = 0.0
@@ -13,26 +18,23 @@ class __Sample():
 
     def __exit__(self, *args):
         self.endTime = wpilib.getTime()
-        __completeSamples.append(self)
+        global _completeSamples
+        _completeSamples.append(self)
 
-
-# Data for the state of this module - dont mess with please
-# accumulates samples recorded with profileRegion and profileFn
-__completeSamples: list[__Sample] = []
 
 # dumps all accumulated samples in the a file named perfLog.csv
 def flushToFile() -> None:
     f = open("perfLog", 'w')
     f.write(f"name, start, end\n")
-    for sample in __completeSamples:
+    for sample in _completeSamples:
         f.write(f"{sample.functionName}, {sample.startTime}, {sample.endTime}\n")
     f.close()
-    __completeSamples.clear()
+    _completeSamples.clear()
 
 def profileFn(fn: Callable) -> Callable:
-    def inner():
+    def inner(*args, **kwargs):
         with __Sample(fn.__name__):
-            fn()
+            fn(*args, **kwargs)
     return inner
 
 def profileRegion(name: str) -> __Sample:
